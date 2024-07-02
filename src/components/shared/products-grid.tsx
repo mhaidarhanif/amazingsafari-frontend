b735fa0fb3a265ee8e5c2a0233aa05af819c8e65
M Haidar Hanif <git@mhaidarhanif.com>
2024-07-02 22:15:33 +0700

    feat: products grid with card and currency

--- src/components/shared/products-grid.tsx
+++ src/components/shared/products-grid.tsx
@@ -1,22 +1,31 @@
 import { Link } from "react-router-dom";
 
 import { Product } from "@/types";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { convertToIDR } from "@/libs/currency";
 
 export function ProductsGrid({ products }: { products: Product[] }) {
   return (
-    <ul>
+    <ul className="grid grid-cols-4 gap-4 max-w-5xl">
       {products.map((product) => (
         <li key={product.id}>
           <Link to={`/products/${product.slug}`}>
-            <div>
-              <img
-                src={product.imageURL}
-                alt={product.name}
-                width={200}
-                height={200}
-              />
-              <h4>{product.name}</h4>
-            </div>
+            <Card>
+              <CardHeader>
+                <img
+                  src={product.imageURL}
+                  alt={product.name}
+                  width={200}
+                  height={200}
+                  className="rounded-t-lg w-full object-contain"
+                />
+              </CardHeader>
+
+              <CardContent>
+                <h4 className="text-lg font-bold">{product.name}</h4>
+                <p>{convertToIDR(product.price)}</p>
+              </CardContent>
+            </Card>
           </Link>
         </li>
       ))}
